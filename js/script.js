b94b1f78ce1ee88a63d79c359e6e6d61f5dfe7ae
Odya01 <republiquez25@gmail.com>
2024-02-26 21:42:44 +0300

    model car func

--- js/script.js
+++ js/script.js
@@ -16,3 +16,42 @@ function init(){
 	myMap.geoObjects.add(myPlacemark); 
 }
 // end map
+
+
+// open modelMenu
+
+const modelParent = document.querySelector('.models');
+const linkModel = document.querySelectorAll('.model__link');
+const modelContent = document.querySelectorAll('.modelCar__content');
+
+function hideTabContent(){
+	modelContent.forEach(item => {
+		item.classList.add('hide');
+		item.classList.remove('show');
+	});
+}
+
+
+function showTabContent(i = 0) {
+	modelContent[i].classList.add('show');
+	modelContent[i].classList.remove('hide');
+}
+
+
+modelParent.addEventListener('click', (event) =>{
+	const target = event.target;
+
+	if (target && target.classList.contains('model__link')) {
+		linkModel.forEach((item, i)=>{
+			if (target == item){
+				console.log(i);
+				hideTabContent();
+				showTabContent(i);
+			}
+		});
+	}
+});
+
+
+// Перебрать и узнать какой элемент 
+
